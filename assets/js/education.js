9a88152deef257c53703d0ae4dc8f6e2294ec68c
vannakne <kannavkat2016@gmail.com>
2022-10-01 15:05:39 +0700

    modified education page

--- assets/js/education.js
+++ assets/js/education.js
@@ -5,66 +5,44 @@ AOS.init();
 const moocs = document.querySelector(".moocs");
 const moocscards = [
   {
-    title: "Data Science",
+    title: "Robot Framework Test Automation: Level 1 (Selenium)",
+    cardImage: "assets/images/education-page/LinkedInLearning.png",
+    moocLink: "https://www.linkedin.com/learning/robot-framework-test-automation-level-1-selenium",
+  },
+  {
+    title: "Unit Testing and Test Driven Development in Python",
+    cardImage: "assets/images/education-page/LinkedInLearning.png",
+    moocLink: "https://www.linkedin.com/learning/unit-testing-and-test-driven-development-in-python",
+  },
+  {
+    title: "Agile Foundations",
+    cardImage: "assets/images/education-page/LinkedInLearning.png",
+    moocLink: "https://www.linkedin.com/learning/agile-foundations",
+  },
+  {
+    title: "Agile Development Practices",
+    cardImage: "assets/images/education-page/LinkedInLearning.png",
+    moocLink: "https://www.linkedin.com/learning/agile-development-practices",
+  },
+  {
+    title: "Introduction to DevOps",
     cardImage: "assets/images/education-page/coursera2.svg",
-    moocLink: "https://www.coursera.org/browse/data-science",
+    moocLink: "https://www.coursera.org/learn/intro-to-devops",
   },
   {
-    title: "Cryptography",
-    cardImage: "assets/images/education-page/udemy.svg",
-    moocLink: "https://www.udemy.com/course/cryptography-for-beginners/",
-  },
-  {
-    title: "Machine Learning",
+    title: "Introduction to Cloud Computing",
     cardImage: "assets/images/education-page/coursera2.svg",
-    moocLink: "https://www.coursera.org/learn/machine-learning",
+    moocLink: "https://www.coursera.org/learn/introduction-to-cloud",
   },
   {
-    title: "Introduction to HTML 5",
-    cardImage: "assets/images/education-page/udacity.svg",
-    moocLink: "https://www.udacity.com/course/intro-to-html-and-css--ud001",
-  },
-  {
-    title: "Introduction to CSS 3",
-    cardImage: "assets/images/education-page/udacity.svg",
-    moocLink: "https://www.udacity.com/course/intro-to-html-and-css--ud001",
-  },
-  {
-    title: "Javascript",
-    cardImage: "assets/images/education-page/udacity.svg",
-    moocLink: "https://www.udacity.com/course/intro-to-javascript--ud803",
-  },
-  {
-    title: "Bootstrap 4",
-    cardImage: "assets/images/education-page/udemy.svg",
-    moocLink: "https://www.udemy.com/course/bootstrap-4-tutorials/",
-  },
-  {
-    title: "Intro to React",
-    cardImage: "assets/images/education-page/edx.svg",
-    moocLink: "https://www.edx.org/learn/reactjs",
-  },
-  {
-    title: "Intro to React Native",
+    title: "Getting Started with Git and GitHub",
     cardImage: "assets/images/education-page/coursera2.svg",
-    moocLink:
-      "https://www.coursera.org/lecture/react-native/introduction-to-react-native-Eax0D",
+    moocLink: "https://www.coursera.org/learn/getting-started-with-git-and-github",
   },
   {
-    title: "NodeJS, Express and MongoDB",
+    title: "Introduction to Containers w/ Docker, Kubernetes & OpenShift",
     cardImage: "assets/images/education-page/coursera2.svg",
-    moocLink: "https://www.coursera.org/learn/server-side-nodejs",
-  },
-  {
-    title: "XML-AJAX",
-    cardImage: "assets/images/education-page/udemy.svg",
-    moocLink: "https://www.udemy.com/course/xml-from-beginner-to-expert/",
-  },
-  {
-    title: "Data Structures & Algorithms",
-    cardImage: "assets/images/education-page/udacity.svg",
-    moocLink:
-      "https://www.udacity.com/course/data-structures-and-algorithms-nanodegree--nd256",
+    moocLink: "https://www.coursera.org/learn/ibm-containers-docker-kubernetes-openshift",
   },
 ];
 
